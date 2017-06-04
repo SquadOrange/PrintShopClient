594f69eebea52b80b190db60204786bf8b60447d
xiangcatherine <xiang.catherine@gmail.com>
2017-06-04 15:17:33 -0400

    Hides and shows forms to make user experience smoother

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -14,9 +14,13 @@ const signUpFailure = (response) => {
 const signInSuccess = (data) => {
   $('.text-display').text('Thanks for signing in. Enjoy!')
   $('.showAfterLogin').show()
-  $('.hideAfterLogin').hide()
+  // $('.hideAfterLogin').hide()
   $('input').val('')
   $('.cartHas-display').empty()
+  $('.sign-in-area').addClass('hidden')
+  $('#change-pwd').removeClass('hidden')
+  $('#sign-out').removeClass('hidden')
+  document.getElementById('sign-in').reset()
 
   // store the user object as per belows
   store.user = data.user
@@ -28,25 +32,46 @@ const signInFailure = () => {
 }
 
 const changePasswordSuccess = (data) => {
-  $('.text-display').text('Password Changed Succesfuly')
+  $('.text-display').text('Password Changed Successfully')
+  $('#change-password').addClass('hidden')
+  document.getElementById('change-password').reset()
   $('input').val('')
 }
 
 const changePasswordFailure = () => {
   $('.text-display').text('Error Changing Password')
+  document.getElementById('change-password').reset()
   $('input').val('')
 }
 
 const signOutSuccess = (data) => {
   $('.text-display').text('Thanks for shopping. Come back soon!')
   $('.hideBeforeLogin').hide()
-  $('.hideAfterLogin').show()
+  // $('.hideAfterLogin').show()
+  $('.sign-in-area').removeClass('hidden')
+  $('#log-in').removeClass('hidden')
+  $('#register').removeClass('hidden')
+  $('#change-pwd').addClass('hidden')
+  $('#change-password').addClass('hidden')
+  $('#sign-out').addClass('hidden')
+  if (!$('#sign-in').hasClass('hidden')) {
+    $('#sign-in').addClass('hidden')
+  }
+  if (!$('#sign-up').hasClass('hidden')) {
+    $('#sign-up').addClass('hidden')
+  }
+  if (!$('.change-pwd-success').hasClass('hidden')) {
+    $('.change-pwd-success').addClass('hidden')
+  }
+  if (!$('.change-pwd-error').hasClass('hidden')) {
+    $('.change-pwd-error').addClass('hidden')
+  }
   // store the user with a value of null as per below
   store.user = null
 }
 
-const signOutFailure = () => {
-  $('.text-display').text('Failure Signing Out')
+const signOutFailure = (response) => {
+  $('.text-display').text('Sorry, unable to sign out.')
 }
 
 const emptyCartSuccess = (data) => {
